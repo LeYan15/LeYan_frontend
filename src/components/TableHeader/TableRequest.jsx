2f3ac8d152f2ba6fd4cd3953bc35fd535646e200
Olga Tabisheva <oliatabisheva@gmail.com>
2023-10-01 16:56:09 +0300

    add main page

--- src/components/TableHeader/TableRequest.jsx
+++ src/components/TableHeader/TableRequest.jsx
@@ -1,22 +1,54 @@
 import './TableRequest.css'
 import TableRequestRow from "../TableRequestRow/TableRequestRow.jsx";
+import down from "../../images/chevron-down.svg";
 
 function TableRequest () {
   return (
-      <section className='table'>
-        <table>
+      <section >
+        <table className='table'>
           <thead>
           <tr>
-            <th>ТК</th>
-            <th>Группа товаров</th>
-            <th>Категория</th>
-            <th>Подкатегория</th>
-            <th>Наименование</th>
-            <th>Неделя</th>
-            <th>Продажи Факт</th>
-            <th>Прогноз</th>
-            <th>Разница</th>
-            <th>Качество прогноза</th>
+            <th><div className='table__box'>ТК<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Группа товаров<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Категория<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Подкатегория<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Наименование<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Неделя<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Продажи Факт<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Прогноз<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Разница<button className='table__button'>
+              <img className='' src={down} alt="альтернативный текст"/>
+
+            </button></div></th>
+            <th><div className='table__box'>Качество прогноза
+              по WAPE  <button className='table__button'>
+                <img className='' src={down} alt="альтернативный текст"/>
+
+              </button></div></th>
           </tr>
           </thead>
           <tbody>
--- src/components/TableSale/TableSale.jsx
+++ src/components/TableSale/TableSale.jsx
@@ -1,19 +1,38 @@
 //import {useNavigate} from "react-router-dom";
 import './TableSale.css'
 import TableSaleRow from "../TableSaleRow/TableSaleRow.jsx";
+import down from "../../images/chevron-down.svg";
 
 function TableSale () {
   return (
-    <section className='table'>
-      <table>
+    <section >
+      <table className='table'>
         <thead>
         <tr>
-          <th>ТК</th>
-          <th>Группа товаров</th>
-          <th>Категория</th>
-          <th>Подкатегория</th>
-          <th>Наименование</th>
-          <th>Ед. измерения</th>
+          <th><div className='table__box'>ТК<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div></th>
+          <th><div className='table__box'>Группа товаров<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div></th>
+          <th><div className='table__box'>Категория<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div></th>
+          <th><div className='table__box'>Подкатегори<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div>я</th>
+          <th><div className='table__box'>Наименование<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div></th>
+          <th><div className='table__box'>Ед. измерения<button className='table__button'>
+            <img className='' src={down} alt="альтернативный текст"/>
+
+          </button></div></th>
         </tr>
         </thead>
         <tbody>
