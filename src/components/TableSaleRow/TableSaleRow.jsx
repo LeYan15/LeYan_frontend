00bcb08fc3076112b49f114253508e5738a95b83
Olga Tabisheva <oliatabisheva@gmail.com>
2023-10-05 21:25:03 +0300

    modification table

--- src/components/TableSaleRow/TableSaleRow.jsx
+++ src/components/TableSaleRow/TableSaleRow.jsx
@@ -1,34 +1,34 @@
 import './TableSaleRow.css'
 
-function TableSaleRow () {
+function TableSaleRow() {
   return (
 
-      <tr>
-        <td className='table__checkbox'>
-          <input id="html" type="checkbox"/>
-        </td>
-        <td className='table__tk'>1</td>
-        <td className='table__group'>Товары собственного приозводства</td>
-        <td className='table__group'>Пироги</td>
-        <td className='table__group'>Пироги</td>
-        <td className='table__group'>Пироги</td>
-        <td className='table__dim'>Пироги</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-        <td className='table__data'>0</td>
-      </tr>
+    <tr>
+      <td className='table__checkbox'>
+        <input id="html" type="checkbox"/>
+      </td>
+      <td className='table__tk'>1</td>
+      <td className='table__group'>Товары собственного приозводства</td>
+      <td className='table__group'>Пироги</td>
+      <td className='table__group'>Пироги</td>
+      <td className='table__group'>Пироги</td>
+      <td className='table__num'>КГ</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
+      <td className='table__count'>0</td>
 
+    </tr>
 
 
   )
